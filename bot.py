3f916c6598c706af2c3f98a716d6328db5ec11b9
popenkoti783-ship-it <popenkoti783@gmail.com>
2026-05-11 19:36:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,56 @@
 import telebot
+import requests
 
 TOKEN = "8218056063:AAEq8B0f4zTM10nlr5NBI4bUT9ljp46zl0Y"
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(
         message.chat.id,
-        "Бот работает 🚀\n\nОтправь ИНН."
+        "Бот работает 🚀\n\nОтправь ИНН компании."
     )
+
+
+def get_company_info(inn):
+    url = f"https://egrul.itsoft.ru/{inn}.json"
+
+    try:
+        response = requests.get(url, timeout=10)
+
+        if response.status_code != 200:
+            return "Компания не найдена."
+
+        data = response.json()
+
+        if "СвЮЛ" not in data:
+            return "Информация не найдена."
+
+        company = data["СвЮЛ"]
+
+        name = company.get("СвНаимЮЛ", {}).get("НаимЮЛПолн", "Нет названия")
+
+        ogrn = company.get("ОГРН", "Нет ОГРН")
+
+        return f"""
+🏢 Компания:
+{name}
+
+🆔 ИНН:
+{inn}
+
+📄 ОГРН:
+{ogrn}
+"""
+
+    except Exception as e:
+        return f"Ошибка: {e}"
+
 
 @bot.message_handler(func=lambda m: True)
 def handle_inn(message):
-
     inn = message.text.strip()
 
     if not inn.isdigit():
@@ -23,9 +60,12 @@ def handle_inn(message):
         )
         return
 
+    result = get_company_info(inn)
+
     bot.send_message(
         message.chat.id,
-        f"Поиск информации по ИНН: {inn}"
+        result
     )
+
 
 bot.infinity_polling()
